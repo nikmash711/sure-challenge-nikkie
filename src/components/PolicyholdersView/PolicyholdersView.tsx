80d964f85364046d5764600bd6a8d0d29731614d
Nikkie Mashian <nikkiemashian@gmail.com>
2022-10-02 17:07:31 -0700

    render the results from the get request as tables

--- src/components/PolicyholdersView/PolicyholdersView.tsx
+++ src/components/PolicyholdersView/PolicyholdersView.tsx
@@ -1,22 +1,76 @@
 import axios from 'axios';
+import { Box } from '@mui/material';
 import { useEffect, useState } from 'react';
 
-function PolicyholdersView() {
-  const [data, setData] = useState(null);
+import InfoTable from '../InfoTable';
 
-  const getData = async () => {
-    let response = await axios.get(
+type PolicyholdersType = {
+  name: string;
+  age: number;
+  address: {
+    line1: string;
+    line2: string | undefined;
+    city: string;
+    state: string;
+    postalCode: string;
+  };
+  phoneNumber: string;
+  isPrimary: boolean;
+};
+
+type GetPolicyholdersResponse = {
+  policyHolders: PolicyholdersType[];
+};
+
+function PolicyholdersView() {
+  const [policyholders, setPolicyholders] = useState<
+    PolicyholdersType[] | undefined
+  >();
+
+  // TODO: error and loading
+  const getPolicyholders = async () => {
+    let { data } = await axios.get<GetPolicyholdersResponse>(
       'https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders'
     );
-    setData(response?.data);
+    setPolicyholders(data.policyHolders);
   };
 
   // On mount, make a GET request to the /api/policyholders endpoint
   useEffect(() => {
-    getData();
+    getPolicyholders();
   }, []);
 
-  return null;
+  return policyholders?.length ? (
+    <Box sx={{ textAlign: 'center' }}>
+      {policyholders.map((policyholder) => {
+        const { name, age, address, phoneNumber, isPrimary } = policyholder;
+        const rows = [
+          { key: 'Name', value: name },
+          { key: 'Age', value: age },
+          {
+            key: 'Address',
+            value: `${address.line1} ${address.line2 ? address.line2 : ''}, ${
+              address.city
+            }, ${address.state} ${address.postalCode}`,
+          },
+          { key: 'Phone number', value: phoneNumber },
+          {
+            key: 'Primary policyholder',
+            value: isPrimary ? 'Yes' : 'No',
+          },
+        ];
+        return (
+          <InfoTable
+            key={`${name}-${phoneNumber}`}
+            header={policyholder.name}
+            rows={rows}
+          />
+        );
+      })}
+    </Box>
+  ) : (
+    <>Empty state would go here</>
+  );
 }
 
 export default PolicyholdersView;
