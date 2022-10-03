bf05b7518fd6d54d7fd9b6e2647b8c9341887d29
Nikkie Mashian <nikkiemashian@gmail.com>
2022-10-03 09:08:35 -0700

    add btn to create policy holder and make post request with fake data

--- src/components/PolicyholdersView/PolicyholdersView.tsx
+++ src/components/PolicyholdersView/PolicyholdersView.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { useEffect, useState } from 'react';
 
 import InfoTable from '../InfoTable';
@@ -35,41 +35,70 @@ function PolicyholdersView() {
     setPolicyholders(data.policyHolders);
   };
 
+  const createPolicyholder = async () => {
+    // Hardcoding this data for the purpose of this assignment.
+    const fakeData = {
+      name: 'Mr. Policy',
+      age: 32,
+      address: {
+        line1: '789 Hold Ave',
+        city: 'Los Angeles',
+        state: 'CA',
+        postalCode: '90035',
+      },
+      phoneNumber: '818-123-4567',
+    };
+    let response = await axios.post(
+      'https://fe-interview-technical-challenge-api-git-main-sure.vercel.app/api/policyholders',
+      fakeData
+    );
+  };
+
   // On mount, make a GET request to the /api/policyholders endpoint
   useEffect(() => {
     getPolicyholders();
   }, []);
 
-  return policyholders?.length ? (
+  return (
     <Box sx={{ textAlign: 'center' }}>
-      {policyholders.map((policyholder) => {
-        const { name, age, address, phoneNumber, isPrimary } = policyholder;
-        const rows = [
-          { key: 'Name', value: name },
-          { key: 'Age', value: age },
-          {
-            key: 'Address',
-            value: `${address.line1} ${address.line2 ? address.line2 : ''}, ${
-              address.city
-            }, ${address.state} ${address.postalCode}`,
-          },
-          { key: 'Phone number', value: phoneNumber },
-          {
-            key: 'Primary policyholder',
-            value: isPrimary ? 'Yes' : 'No',
-          },
-        ];
-        return (
-          <InfoTable
-            key={`${name}-${phoneNumber}`}
-            header={policyholder.name}
-            rows={rows}
-          />
-        );
-      })}
+      {policyholders?.length ? (
+        policyholders.map((policyholder) => {
+          const { name, age, address, phoneNumber, isPrimary } = policyholder;
+          const rows = [
+            { key: 'Name', value: name },
+            { key: 'Age', value: age },
+            {
+              key: 'Address',
+              value: `${address.line1} ${address.line2 ? address.line2 : ''}, ${
+                address.city
+              }, ${address.state} ${address.postalCode}`,
+            },
+            { key: 'Phone number', value: phoneNumber },
+            {
+              key: 'Primary policyholder',
+              value: isPrimary ? 'Yes' : 'No',
+            },
+          ];
+          return (
+            <InfoTable
+              key={`${name}-${phoneNumber}`}
+              header={policyholder.name}
+              rows={rows}
+            />
+          );
+        })
+      ) : (
+        <>Empty state</>
+      )}
+      <Button
+        onClick={createPolicyholder}
+        variant="outlined"
+        color="primary"
+        size="large"
+      >
+        Add a policyholder
+      </Button>
     </Box>
-  ) : (
-    <>Empty state would go here</>
   );
 }
 
